aab23f95ae6e6c8725ff9cfaa1e64b53ebe6ab49
Alizamin Jafarli <jafarli_alizamin@yahoo.com>
2020-06-22 23:28:46 +0200

    Create print.py

---
+++ hackerrank/print.py
@@ -0,0 +1,5 @@
+#https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i, end="")
