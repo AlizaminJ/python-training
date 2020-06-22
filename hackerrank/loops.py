3c7472d42452e04976c378b43ed5a60034511133
Alizamin Jafarli <jafarli_alizamin@yahoo.com>
2020-06-22 22:38:57 +0200

    Create loops.py

---
+++ hackerrank/loops.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    i=0
+    while i<n:
+        print(i**2)
+        i+=1
+
+
+#if __name__ == '__main__':
+#    n = int(raw_input())
+#    for i in range(n):
+#        print (i**2)
