1565636aa0d2704e40468e3e32d6e6b0d8cdbe70
Alizamin Jafarli <jafarli_alizamin@yahoo.com>
2020-06-24 23:48:49 +0200

    Create np-shape.py

---
+++ hackerrank/np-shape.py
@@ -0,0 +1,3 @@
+#https://www.hackerrank.com/challenges/np-shape-reshape/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+import numpy as np
+print(np.array(input().split(),int).reshape(3,3))
