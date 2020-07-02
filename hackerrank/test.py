cb0c8bd6c8f26f02194147d1e4db318e6773318f
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-07-02 23:29:58 +0200

    regex

--- hackerrank/test.py
+++
@@ -1,7 +0,0 @@
-# https://www.hackerrank.com/challenges/np-linear-algebra/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
-import numpy as np
-np.set_printoptions(legacy='1.13')
-
-n=int(input())
-a=np.array([input().split(' ') for _ in range(n)], float)
-print(np.linalg.det(a))
