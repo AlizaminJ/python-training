4c681ca98e5f0faadd47b2fb4df10df520c3ecae
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-07-18 23:47:49 +0200

    second solution

--- hackerrank/nested-lists.py
+++ hackerrank/nested-lists.py
@@ -1,13 +1,22 @@
 # https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
+#marksheet = []
+#for _ in range(0,int(input())):
+#    marksheet.append([input(), float(input())])
+#
+#second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+#print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
+
+marksheet=[]
+scorelist=[]
 if __name__ == '__main__':
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        students=[]
-        students.append([name, score])
-        lowest=min(score)
-        for items in students:
-            if lowest==min(score):
-                students.remove(item)
-        print(name, min(score))
+        for _ in range(int(input())):
+                name = input()
+                score = float(input())
+                marksheet+=[[name,score]]
+                scorelist+=[score]
+        b=sorted(list(set(scorelist)))[1] 
+
+        for a, c in sorted(marksheet):
+             if c==b:
+                print(a)
