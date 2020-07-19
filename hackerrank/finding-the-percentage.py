86b434543de257708889335c2191a46b37a67e1c
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-07-19 23:41:07 +0200

    finding the percentage

---
+++ hackerrank/finding-the-percentage.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    result=sum(student_marks[query_name])/len(student_marks[query_name])
+    print("{:.2f}".format(result))
