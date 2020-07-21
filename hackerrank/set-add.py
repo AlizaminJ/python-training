b4a768d82922f30182b4c4b8caa79301638847fe
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-07-21 23:41:02 +0200

    set-add

---
+++ hackerrank/set-add.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/py-set-add/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+s = set()
+for _ in range(int(input())):
+    s.add(input())
+print(len(s))
+
+# print(len(set([str(input()) for _ in range(int(input()))])))
