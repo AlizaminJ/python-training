220102dc79b3303865fa76185db7fb3635d4ea5e
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-07-27 12:45:53 +0200

    captains-room

---
+++ hackerrank/captains-room.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/py-the-captains-room/problem?h_r=next-challenge&h_r%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D=next-challenge&h_v=zen&h_v%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+K = int(input())
+rooms = input().split()
+rooms.sort()
+capt_room = (set(rooms[0::2]) ^ set(rooms[1::2]))
+print(capt_room.pop())
+
+# k, arr = int(input()),list(map(int, input().split()))
+# myset = set(arr)
+# print(((sum(myset)*k)-(sum(arr)))//(k-1))
