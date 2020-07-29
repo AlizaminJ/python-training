9f27da4bede802d2d4be842dfe611163cced50a0
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-07-29 23:08:53 +0200

    check strict superset

---
+++ hackerrank/check-strict-superset.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/py-check-strict-superset/problem?h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&isFullScreen=true&h_r=next-challenge&h_v=zen
+
+#a = set(input().split())
+#print(all(a > set(input().split()) for _ in range(int(input()))))
+
+#seta = set(map(input().split()))
+#print(all([seta.issuperset(set(map(int, input().split()))) for i in range(int(input()))]))
+
+a = set(map(str, input().split(' ')))
+is_strict_superset = []
+for i in range(int(input())):
+     is_strict_superset.append(a.issuperset(set(map(str, input().split(' ')))))
+print(ll(is_strict_superset))
