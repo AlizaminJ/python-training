3721a4561dca432d46e057a49a639bbac5540e24
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-08-04 23:45:42 +0200

    matrix script

---
+++ hackerrank/matrix-script.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/matrix-script/problem?h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+import re
+
+n, m = map(int, input().split())
+a, b = [], ""
+for _ in range(n):
+    a.append(input())
+
+for z in zip(*a):
+    b += "".join(z)
+
+print(re.sub(r"(?<=\w)([^\w]+)(?=\w)", " ", b))
+
+# x,y = list(map(int,input().split()))
+# rows =[input() for i in range(x)]
+# text = "".join([row[i] for i in range(y) for row in rows])
+# text = re.sub('([A-Za-z1-9])[^A-Za-z1-9]+([A-Za-z1-9])', r'\1 \2', text)
+# text = re.sub('  ', ' ', text)
+# print(text)
+
