54625abecfc07a0cf03926ee7df5ef6bf9c3d1c8
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-08-06 23:44:48 +0200

    design-door-map

---
+++ hackerrank/designer-door-map.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/designer-door-mat/problem?h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+N, M = map(int,input().split())
+for i in range(1,N,2): 
+    print((i * ".|.").center(M, "-"))
+print("WELCOME".center(M,"-"))
+for i in range(N-2,-1,-2): 
+    print((i * ".|.").center(M, "-"))
+
+# n, m = map(int,input().split())
+# pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
+# print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
