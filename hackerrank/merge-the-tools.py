701e29bb1de7cc12ebb32c9d4cc3ebcb0e3cbb13
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-08-14 23:34:24 +0200

    merge the tool 2

--- hackerrank/merge-the-tools.py
+++ hackerrank/merge-the-tools.py
@@ -1,10 +1,26 @@
 # https://www.hackerrank.com/challenges/merge-the-tools/problem?h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
-from collections import OrderedDict
+# from collections import OrderedDict
+
+# def merge_the_tools(string, k):
+#     for x in range(0,len(string),k):     
+#         print(''.join(list(OrderedDict.fromkeys(string[x:x+k]))))
+# if __name__ == '__main__':
+#     string, k = input(), int(input())
+#     merge_the_tools(string, k)
+
 
 def merge_the_tools(string, k):
-    for x in range(0,len(string),k):     
-        print(''.join(list(OrderedDict.fromkeys(string[x:x+k]))))
+    temp = []
+    len_temp = 0
+    for item in string:
+        len_temp += 1
+        if item not in temp:
+            temp.append(item)
+        if len_temp == k:
+            print (''.join(temp))
+            temp = []
+            len_temp = 0
 if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
