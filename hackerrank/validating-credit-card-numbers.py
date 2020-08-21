693d367d2dd616a770fd2d84a0671d86b100aa1d
AlizaminJ <jafarli_alizamin@yahoo.com>
2020-08-21 23:46:08 +0200

    validating credit card

---
+++ hackerrank/validating-credit-card-numbers.py
@@ -0,0 +1,20 @@
+# https://www.hackerrank.com/challenges/validating-credit-card-number/problem?h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_r%5B%5D%5B%5D%5B%5D=next-challenge&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&h_v%5B%5D%5B%5D%5B%5D=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import re
+pattern = re.compile(
+    r"^" # beginning of the string
+    r"(?!.*(\d)(-?\1){3})" # digits in groups of 4 separated by one hyphen "-"
+    r"[456]" # must start with 4, 5 or 6
+    r"\d{3}" # no 4 or more consecutive digits
+    r"(?:-?\d{4}){3}"  # contain exactly 16 digits
+    r"$") # end of the string
+for _ in range(int(input())):
+    print("Valid" if pattern.search(input()) else "Invalid")
+
+# import re
+# for _ in range(int(input())):
+#     s = input()
+#     if re.match(r"^[456]([\d]{15}|[\d]{3}(-[\d]{4}){3})$", s) and not re.search(r"([\d])\1\1\1", s.replace("-", "")):
+#         print("Valid")
+#     else:
+#         print("Invalid")
